6cffe74446baab9e372cd812ea49ecd236e76237
luiscds <43537179+luiscds@users.noreply.github.com>
2018-10-15 12:22:27 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,32 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //Coloque aqui o seu codigo
+            double x, y;
+            Console.WriteLine("insira as codernadas de x");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("insira as codernadas de y");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            if(x>0 && y>0)
+            {
+                Console.WriteLine("x-{0} e y-{1} Pertencem ao 1º quadrante", x, y);
+            }
+            else if(x>0 && y<0) 
+            {
+                Console.WriteLine("x-{0} e y-{1} Pertencem ao 4º quadrante", x, y);
+            }
+             else if(x<0 && y>0)
+            {
+                Console.WriteLine("x-{0} e y-{1} Pertencem ao 2º quadrante", x, y);
+            }
+            else if(x<0 && y<0)
+            {
+                Console.WriteLine("x-{0} e y-{1} Pertencem ao 3º quadrante", x, y);
+            }
+             else if(x<0 && y<0)
+            {
+                Console.WriteLine("x-{0} e y-{1} Pertencem a origem do referencial ", x, y);
+            }
         
         }
     }
